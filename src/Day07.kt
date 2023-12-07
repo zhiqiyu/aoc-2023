2b3e1e579aa6a14ffcebbdab0d604d37a878fafa
zhiqiyu <yzq1027@hotmail.com>
2023-12-07 18:36:31 -0500

    day 7 part1

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,3 +1,53 @@
+class Comparator() {
+    val order = listOf('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A')
+    val counts = getCardCounts()
+
+    fun getHighestLevel(): Int {
+        if (hasFive()) return 7
+        if (hasFour()) return 6
+        if (hasFullHouse()) return 5
+        if (hasThree()) return 4
+        if (hasTwo()) return 3
+        if (hasOne()) return 2
+        if (hasHighCard()) return 1
+        return 0
+    }
+
+    fun hasFive(): Boolean {
+        return input.toSet().size == 1
+    }
+
+    fun hasFour(): Boolean {
+        return input.toSet().size == 2 && counts.values.indexOf(1) >= 0
+    }
+
+    private fun getCardCounts(): MutableMap<Char, Int> {
+        var counts = mutableMapOf<Char, Int>()
+        input.forEach { counts.set(it, counts.getOrDefault(it, 0) + 1)}
+        return counts
+    }
+
+    fun hasFullHouse(): Boolean {
+        return counts.values.indexOf(3) >= 0 && counts.size == 2
+    }
+
+    fun hasThree(): Boolean {
+        return counts.values.indexOf(3) >= 0
+    }
+
+    fun hasTwo(): Boolean {
+        return counts.size == 3 && counts.values.indexOf(1) >= 0
+    }
+
+    fun hasOne(): Boolean {
+        return counts.size == 4
+    }
+
+    fun hasHighCard(): Boolean {
+        return counts.size == 5
+    }
+}
+
 class Hand(val input: String, val bid: Double) {
     val order = listOf('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A')
     val counts = getCardCounts()
@@ -32,7 +82,7 @@ class Hand(val input: String, val bid: Double) {
     }
 
     fun hasFour(): Boolean {
-        return input.toSet().size == 2
+        return input.toSet().size == 2 && counts.values.indexOf(1) >= 0
     }
 
     private fun getCardCounts(): MutableMap<Char, Int> {
@@ -79,24 +129,24 @@ fun main() {
                 when (r) {
                     1 -> points[i] += 1.0
                     -1 -> points[j] += 1.0
+                    else -> println("!!WRONG")
                 }
             }
         }
-        print(points.toSet().size)
         for (i in input.indices) {
             result += hands[i].bid * points[i]
         }
         return result
     }
 
-    // fun part2(input: List<String>): Long {
+    fun part2(input: List<String>): Long {
         
         
-    // }
+    }
 
     val testInput = readInput("Day07_test")
 
-    // check(part1(testInput) == 6440.0)
+    check(part1(testInput) == 6440.0)
     // check(part2(testInput) == 71503L)
 
     val input = readInput("Day07")
